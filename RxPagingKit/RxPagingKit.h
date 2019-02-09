8d8f16c405bdb6892476d7d4043877f4ae901b12
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2019-02-09 13:36:23 +0900

    Initial Commit

---
+++ RxPagingKit/RxPagingKit.h
@@ -0,0 +1,19 @@
+//
+//  RxPagingKit.h
+//  RxPagingKit
+//
+//  Created by kahayash on 2019/02/09.
+//  Copyright © 2019 Kazuhiro Hayashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxPagingKit.
+FOUNDATION_EXPORT double RxPagingKitVersionNumber;
+
+//! Project version string for RxPagingKit.
+FOUNDATION_EXPORT const unsigned char RxPagingKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxPagingKit/PublicHeader.h>
+
+
